1efe3632323e0bb7a745eec54a478114c5d056e3
updhull <abdullahi.ismail@student.moringaschool.com>
2021-11-21 17:15:53 +0300

    This will add toggle effect in what we do section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#image").click(function(){
+        $("#image").toggle();
+        $("#design").toggle();
+    });
+    $(document).ready(function(){
+        $("#image1").click(function(){
+            $("#image1").toggle();
+            $("#development").toggle();
+        });
+    $(document).ready(function(){
+        $("#image2").click(function(){
+            $("#image2").toggle();
+            $("#management").toggle();
+        });
+    });
+});
+});
+
+ 
