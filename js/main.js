dde06c57873f5e137f1bace85cbd68c0abdc70b0
updhull <abdullahi.ismail@student.moringaschool.com>
2021-11-22 15:24:37 +0300

    This will add hover effect in portfolio section

--- js/main.js
+++ js/main.js
@@ -19,10 +19,64 @@ $(document).ready(function(){
             $("#management").click(function(){
                 $(".six").toggle();
             });
-           
+            
+
         });
     });
 });
 });
+// Portfolio overlays
+$(document).ready(function(){
+    $(".work1").mouseover(function(){
+        $("#ten").show();
+    });
+    $(".work1").mouseout(function(){
+        $("#ten").hide();
+    });
 
- 
+    $(".work2").mouseover(function(){
+        $("#eleven").show();
+    });
+    $(".work2").mouseout(function(){
+        $("#eleven").hide();
+    });
+    $(".work3").mouseover(function(){
+        $("#twelve").show();
+    });
+    $(".work3").mouseout(function(){
+        $("#twelve").hide();
+    });
+    $(".work4").mouseover(function(){
+        $("#thirteen").show();
+    });
+    $(".work4").mouseout(function(){
+        $("#thirteen").hide();
+    });
+    $(".work5").mouseover(function(){
+        $("#fourteen").show();
+    });
+    $(".work5").mouseout(function(){
+        $("#fourteen").hide();
+    });
+    $(".work6").mouseover(function(){
+        $("#fifteen").show();
+    });
+    $(".work6").mouseout(function(){
+        $("#fifteen").hide();
+    });
+    $(".work7").mouseover(function(){
+        $("#sixteen").show();
+    });
+    $(".work7").mouseout(function(){
+        $("#sixteen").hide();
+    });
+    $(".work8").mouseover(function(){
+        $("#seventeen").show();
+    });
+    $(".work8").mouseout(function(){
+        $("#seventeen").hide();
+    });
+});
+
+
+
